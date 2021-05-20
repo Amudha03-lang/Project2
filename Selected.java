ecab34f9cca773d0299d529e0c0c8d977e08609b
Amudha03-lang <81177896+Amudha03-lang@users.noreply.github.com>
2021-05-20 21:32:49 +0530

    Initial Commit

---
+++ Selected.java
@@ -0,0 +1,38 @@
+package launch_browser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selected {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.setProperty("webdriver.chrome.driver","E:\\Software Testing\\Selenium\\Driver\\chromedriver.exe" );
+		
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://shareselenium.herokuapp.com/CheckBox.html");
+		
+		try {
+			
+			WebElement element = driver.findElement(By.xpath("(//input[@type='checkbox'])[5]"));
+			
+			if(element.isSelected()) {
+				System.out.println("Element is selected");
+			}
+			else {
+				System.out.println("Element is not selected");
+				element.click();
+			}
+		}
+		
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
