b6a3adf23a76ce67b9248a9f77d5053683cfd04d
Amudha03-lang <81177896+Amudha03-lang@users.noreply.github.com>
2021-05-21 13:20:12 +0530

    Added first printout statement

--- Selected.java
+++ Selected.java
@@ -9,7 +9,7 @@ public class Selected {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+System.out.println("Start of the program");		
 System.setProperty("webdriver.chrome.driver","E:\\Software Testing\\Selenium\\Driver\\chromedriver.exe" );
 		
 		WebDriver driver= new ChromeDriver();
